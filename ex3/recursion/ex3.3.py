797d497563cf100d76f21f3c2afd91018021797a
Azriel-Ehrlich <azrielye669@gmail.com>
2025-09-16 15:26:15 +0300

    forced a way to make lcm not tail

--- ex3/recursion/ex3.3.py
+++ ex3/recursion/ex3.3.py
@@ -5,16 +5,23 @@ def lcm(a, b):
     def gcd(x, y):
         if y == 0:
             return x
-        return gcd(y, x % y)
+        return gcd(y, x % y)*1
     return abs(a * b) // gcd(a, b)
 
 # find LCM of two numbers recursion with tail
-@tail_recursive
 def lcm_tail(a, b, acc=1):
+    @tail_recursive
     def gcd(x, y):
         if y == 0:
             return x
-        return gcd(y, x % y)
+        return gcd.tail_call(y, x % y)
     if a == 0 or b == 0:
         return 0
     return abs(a * b) // gcd(a, b)
+
+def main():
+    print ("LCM of 12 and 15 is:", lcm(12, 15))
+    print ("LCM of 12 and 15 using tail recursion is:", lcm_tail(12, 15))
+
+if __name__ == "__main__":
+    main()
